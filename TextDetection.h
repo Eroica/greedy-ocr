81b86f7ce71931d3a4029e59de8d645b4dd4a787
Seba <lseba@icloud.com>
2015-06-02 22:22:19 +0200

    add NSValue wrappers for structs

--- TextDetection.h
+++ TextDetection.h
@@ -3,25 +3,87 @@
 
 #include "opencv/cv.h"
 
-
-typedef struct {
+// Point2d struct.
+struct _Point2d {
     int x;
     int y;
     float SWT;
-} Point2d;
+};
+typedef struct _Point2d Point2d;
 
-typedef struct {
+@interface NSValue (Point2d)
++ (id)valueWithPoint2d:(Point2d)point2d;
+- (Point2d)point2dValue;
+@end
+
+@implementation NSValue (Point2d)
++ (id)valueWithPoint2d:(Point2d)point2d
+{
+    return [NSValue value:&point2d withObjCType:@encode(Point2d)];
+}
+
+- (Point2d)point2dValue
+{
+    Point2d point2d;
+    [self getValue:&point2d];
+    return point2d;
+}
+@end
+
+// Point2dFloat struct.
+struct _Point2dFloat {
     float x;
     float y;
-} Point2dFloat;
+};
+typedef struct _Point2dFloat Point2dFloat;
 
-typedef struct {
+@interface NSValue (Point2dFloat)
++ (id)valueWithPoint2dFloat:(Point2dFloat)point2dFloat;
+- (Point2dFloat)point2dFloatValue;
+@end
+
+@implementation NSValue (Point2dFloat)
++ (id)valueWithPoint2dFloat:(Point2dFloat)point2dFloat
+{
+    return [NSValue value:&point2dFloat withObjCType:@encode(Point2dFloat)];
+}
+
+- (Point2dFloat)point2dFloatValue
+{
+    Point2dFloat point2dFloat;
+    [self getValue:&point2dFloat];
+    return point2dFloat;
+}
+@end
+
+// Point3dFloat struct.
+struct _Point3dFloat {
     float x;
     float y;
     float z;
-} Point3dFloat;
+};
+typedef struct _Point3dFloat Point3dFloat;
 
+@interface NSValue (Point3dFloat)
++ (id)valueWithPoint3dFloat:(Point3dFloat)point3dFloat;
+- (Point3dFloat)point3dFloatValue;
+@end
 
+@implementation NSValue (Point3dFloat)
++ (id)valueWithPoint3dFloat:(Point3dFloat)point3dFloat
+{
+    return [NSValue value:&point3dFloat withObjCType:@encode(Point3dFloat)];
+}
+
+- (Point3dFloat)point3dFloatValue
+{
+    Point3dFloat point3dFloat;
+    [self getValue:&point3dFloat];
+    return point3dFloat;
+}
+@end
+
+// Others.
 @interface Ray : NSObject {
 
 }
