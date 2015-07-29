5bf4daf5ae3a8b5302a1847c1a2a5b9374eada9a
Seba <gnu.seba@outlook.com>
2015-07-29 15:58:03 +0200

    add Bag, Ngram classes

--- Python/LanguageModel.py
+++ Python/LanguageModel.py
@@ -1,6 +1,60 @@
 import re
 import nltk
 
+import gr_config as CONFIG
+
+class NgramModel(dict):
+    """
+
+    """
+
+    def __init__(self, text, n=2):
+        """
+        """
+
+        super(NgramModel, self).__init__()
+
+        _ngrams = list(nltk.ngrams(text, n))
+
+        for word_pair in _ngrams:
+            if word_pair[0] not in self:
+                self[word_pair[0]] = Bag()
+
+            for i in range(1, n):
+                self[word_pair[0]].insert(word_pair[i])
+
+
+class Bag(dict):
+    """
+
+    """
+
+    def __init__(self):
+        """
+        """
+
+        super(Bag, self).__init__()
+
+    def insert(self, key):
+        """
+        """
+
+        try:
+            self[key] += 1
+        except KeyError:
+            self[key] = 1
+
+    def remove(self, key):
+        """
+        """
+
+        count = self[key]
+        self[key] = (count and count > 1) and count - 1 or 0
+
+        if self[key] == 0:
+            del self[key]
+
+
 class Lexicon(set):
     """
 
@@ -22,9 +76,8 @@ class Lexicon(set):
 
         return [word.group(0) for word in (re.search(search_term, l) for l in self) if word]
 
-# class TrigramModel()
 
 text = [x.strip() for x in open(CONFIG.MERCURIUS_FILE).read().split()]
 freq = nltk.FreqDist(text)
-bi = list(nltk.bigrams(text))
-tri = list(nltk.ngrams(text, 3))
+bi = NgramModel(text, 2)
+tri = NgramModel(text, 3)
