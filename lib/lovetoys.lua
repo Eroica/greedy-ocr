73e060da9b7c7483506a6a008963744ad4be4d72
Seba <lseba@outlook.de>
2015-09-03 16:34:45 +0200

    add Lua dependencies

---
+++ lib/lovetoys.lua
@@ -0,0 +1,22 @@
+-- Getting folder that contains engine
+local folderOfThisFile = (...):match("(.-)[^%/]+$")
+
+lovetoyDebug = false
+
+-- Requiring class
+
+class = require('src/middleclass')
+
+-- Requiring util functions
+require(folderOfThisFile .. "src/util")
+
+-- Requiring all Events
+require(folderOfThisFile .. "src/events/ComponentAdded")
+require(folderOfThisFile .. "src/events/ComponentRemoved")
+
+-- Requiring the lovetoys
+require(folderOfThisFile .. "src/Entity")
+require(folderOfThisFile .. "src/Engine")
+require(folderOfThisFile .. "src/System")
+require(folderOfThisFile .. "src/EventManager")
+require(folderOfThisFile .. "src/Component")
