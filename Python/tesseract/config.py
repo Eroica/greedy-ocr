d4d430b2fdac43b25eb62d91447db8090e6969fa
Seba <gnu.seba@outlook.com>
2015-09-07 13:16:54 +0200

    remove old code from this branch

--- Python/tesseract/config.py
+++
@@ -1,7 +0,0 @@
-LANGUAGE_MODEL = 'deu-frak'
-LANGUAGE_NAME = 'fraktur'
-
-ALPHABET = list('abcdefghijklmnopqrstuvwxyz')
-IMAGES_PATH = '../share/letters'
-LEXICON = '../share/lexicon.txt'
-EXTENSIONS = ('.jpg', '.png', '.tiff')
