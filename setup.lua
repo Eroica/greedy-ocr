fa7ccd81e637b96177db61313555b0ddd5f6d196
Seba <gnu.seba@outlook.com>
2015-09-08 16:13:24 +0200

    remove unneeded globals

--- setup.lua
+++ setup.lua
@@ -1,16 +1,11 @@
 function load_prototypes()
-    prototypes = {}
-    new_prototypes = {}
-
     for _, prototype in pairs(config.prototypes) do
         local image = love.graphics.newImage(prototype[2])
         local prototype = entities.Prototype(prototype[1], image)
-        table.insert(prototypes, prototype)
     end
 end
 
 function load_image()
     local line_image = love.graphics.newImage(config.line[1])
     line = entities.Line(line_image, config.line.boxes)
-    segments = line._segments
 end
