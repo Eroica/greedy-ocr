e88900226cdcd52dffee21b421e2b4f5d6d59aa4
Seba <gnu.seba@outlook.com>
2015-09-08 16:13:16 +0200

    add  _prototypes, _segments table to engine

--- engines.lua
+++ engines.lua
@@ -7,10 +7,39 @@
 ]]
 
 GreedyEngine = class("GreedyEngine", Engine)
-function GreedyEngine:__init()
-    self.testi = 123
+function GreedyEngine:__init ()
+    self._prototypes = {}
+    self._segments = {}
 
     self:addInitializer("isPrototype", function (entity)
-        print("Prototype `" .. entity:get("String").string .. "' was created.")
+        if config.DEBUG then
+            print("Prototype `" .. entity:get("String").string .. "' was created.")
+        end
+
+        table.insert(self._prototypes, entity)
     end)
+
+    self:addInitializer("isSegment", function (entity)
+        if config.DEBUG then
+            --print("Segment `" .. entity:get("String").string .. "' was created.")
+            print "Segment created"
+        end
+
+        table.insert(self._segments, entity)
+    end)
+end
+
+function GreedyEngine:allPrototypes ()
+    return self._prototypes
+end
+
+function GreedyEngine:checkIfPrototypeExists (literal)
+    for _, p in pairs(self._prototypes) do
+        if p:get("String").string == literal then
+            print "you already exist"
+            return true
+        end
+    end
+
+    return false
 end
