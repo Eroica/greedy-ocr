4b1cfc208e7e35b088ad55b614e9c47960806977
Seba <gnu.seba@outlook.com>
2015-09-10 18:30:52 +0200

    Capitalize Entities

--- setup.lua
+++ setup.lua
@@ -1,11 +1,11 @@
 function load_prototypes()
     for _, prototype in pairs(config.prototypes) do
         local image = love.graphics.newImage(prototype[2])
-        local prototype = entities.Prototype(prototype[1], image)
+        local prototype = Entities.Prototype(prototype[1], image)
     end
 end
 
 function load_image()
     local line_image = love.graphics.newImage(config.line[1])
-    line = entities.Line(line_image, config.line.boxes)
+    line = Entities.Page(line_image, config.line.boxes)
 end
