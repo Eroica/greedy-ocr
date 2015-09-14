1fad12c9c9e384a2e6831390f3dda11544358d0d
Seba <gnu.seba@outlook.com>
2015-09-14 18:19:55 +0200

    make load_protypes() use love.filesystem

--- setup.lua
+++ setup.lua
@@ -1,13 +1,20 @@
-function load_prototypes()
-    local prototypes = {}
+local prototypes_directory = config.prototypes_directory
 
-    for _, prototype in pairs(config.prototypes) do
-        local image = love.graphics.newImage(prototype[2])
-        local prototype = Entities.Prototype(prototype[1], image)
-        table.insert(prototypes, prototype)
+function load_prototypes ()
+    local function create_prototype (filename)
+        if  love.filesystem.isFile(prototypes_directory .. "/" .. filename)
+        and filename:sub(1, 1) ~= "."
+        and filename:sub(1, 1) ~= "_" then
+            local image = love.graphics.newImage(prototypes_directory .. "/" .. filename)
+
+            -- Get the part before the `.'
+            local literal = explode(".", filename)[1]
+
+            Entities.Prototype(literal, image)
+        end
     end
 
-    return prototypes
+    love.filesystem.getDirectoryItems(prototypes_directory, create_prototype)
 end
 
 function load_image()
