33856f873cd46c3a60d2152006eb6e73222def81
Seba <gnu.seba@outlook.com>
2015-09-30 16:52:16 +0200

    add letter frequencies

--- gr_lexicon.sh
+++ gr_lexicon.sh
@@ -4,8 +4,13 @@
 #-----------------------------------------------------------------------
 #   gr_lexicon.sh
 #
-#   tbd
+#   This bash script was used to extract some statistical information
+#   from the Mercurius document (the first half, in this case).
+#   The first 2 lines create a lexicon containing every word that exists
+#   in document, and the 3rd line extracts how often a specific letter
+#   occurs in it.
 
 #!/usr/bin/bash
 cat mercurius_1st_half.txt | tr -cs "[:alpha:]" "\n" | sort | uniq > raw_lexicon.txt
 LC_ALL="de" sort -fd raw_lexicon.txt | uniq > lexicon_1st_half.txt
+cat mercurius_1st_half.txt | fold -w1 | sort | uniq -c | sort -k 1nr > letter_frequencies.txt
