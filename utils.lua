df10db4998fb41e28a334941b98cd041d78656c1
Seba <gnu.seba@outlook.com>
2015-10-03 01:32:07 +0200

    fix trim_image()

--- utils.lua
+++ utils.lua
@@ -30,8 +30,10 @@ end
 -- http://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
 -- (Date retrieved: September 1st, 2015)
 --
--- @params: r, g, b : numbers
+-- @params:  r, g, b: R, G, B values
+--                    @type: number
 -- @returns: A number from 0 to 255.
+--           @type: number
 function rgb2grey (r, g, b)
     return math.ceil(0.21*r + 0.72*g + 0.07*b)
 end
@@ -42,7 +44,10 @@ end
 -- `value' can be specified when calling `threshold'. This creates a
 -- closure!
 --
--- @params: value : number
+-- @params:  value: The threshold value
+--                  @type: number
+-- @returns: A threshold function
+--           @type: function
 function threshold (value)
     local value = value or 127
 
@@ -57,30 +62,41 @@ function threshold (value)
     end
 end
 
+-- threshold_image:
+-- Takes an image and creates a binary image out of it.
+--
+-- @params: image: The image to be thresholded
+--                 @type: Image (love2d's image format)
+-- @returns: @type: Image (love2d's image format)
 function threshold_image (image)
     local width, height = image:getWidth(), image:getHeight()
-    local image_data = love.image.newImageData(width, height)
+    local image_data    = love.image.newImageData(width, height)
     image_data:paste(image:getData(), 0, 0, 0, 0, width, height)
     image_data:mapPixel(threshold())
     return love.graphics.newImage(image_data)
 end
 
-
+-- trim_image:
+-- Cuts away white pixels that make up an image's border.
+--
+-- @params:  image: An image
+--                  @type: Image
+-- @returns: @type: Image
 function trim_image (image)
     local function all_white (t)
         for i=1, #t do
             if t[i] ~= 255 then return false end
-        end
+        end --inner for
 
         return true
     end
 
     local image_bw = threshold_image(image)
-    local width = image_bw:getWidth()
-    local height = image_bw:getHeight()
+    local width    = image_bw:getWidth()
+    local height   = image_bw:getHeight()
 
     local white_columns = {}
-    local white_rows = {}
+    local white_rows    = {}
 
     for i=0, height - 1 do
         local row = {}
@@ -88,13 +104,12 @@ function trim_image (image)
         for j=0, width - 1 do
             local r, g, b = image_bw:getData():getPixel(j, i)
             row[#row + 1] = rgb2grey(r, g, b)
-        end
+        end --inner for
 
         if all_white(row) then
             white_rows[#white_rows + 1] = i
-        end
-
-    end
+        end --inner if
+    end --for
 
     for i=0, width - 1 do
         local column = {}
@@ -102,29 +117,66 @@ function trim_image (image)
         for j=0, height - 1 do
             local r, g, b = image_bw:getData():getPixel(i, j)
             column[#column + 1] = rgb2grey(r, g, b)
-
-        end
+        end --inner for
 
         if all_white(column) then
             white_columns[#white_columns + 1] = i
-        end
-    end
+        end --inner if
+    end --for
 
-    -- check up until what index the array is continuous
+    -- Keep a buffer of n white pixels
     local BUFFER = 0
 
-    local max_row_left = find_successor(white_rows, 1) - BUFFER
-    local max_row_right = math.abs(#white_rows - find_antecessor(white_rows, #white_rows) - BUFFER)
-    local max_column_left = find_successor(white_columns, 1) - BUFFER
-    local max_column_right = math.abs(#white_columns - find_antecessor(white_columns, #white_columns) - BUFFER)
+    local white_pixels_left = 0
+    if #white_columns ~= 0 then
+        white_pixels_left = find_successor(white_columns, 1) - BUFFER
+    end
 
-    local trimmed_image_data = love.image.newImageData(width - max_column_left - max_column_right, height - max_row_left - max_row_right)
-    trimmed_image_data:paste(image:getData(), 0, 0, max_column_left, max_row_left, width - max_column_right, height - max_row_right)
+    local white_pixels_right = 0
+    if white_columns[#white_columns] == width - 1 then
+        white_pixels_right =   #white_columns
+                             - find_antecessor(white_columns, #white_columns)
+                             + 1
+    end
+
+    local white_pixels_top = 0
+    if #white_rows ~= 0 then
+        local white_pixels_top = find_successor(white_rows, 1) - BUFFER
+    end
+
+    local white_pixels_bottom = 0
+    if white_rows[#white_rows] == height - 1 then
+        white_pixels_bottom =   #white_rows
+                              - find_antecessor(white_rows, #white_rows)
+                              + 1
+    end
+
+    local trimmed_width      = width  - white_pixels_left - white_pixels_right
+    local trimmed_height     = height - white_pixels_top  - white_pixels_bottom
+    local trimmed_image_data = love.image.newImageData(trimmed_width,
+                                                       trimmed_height)
+
+    trimmed_image_data:paste(image:getData(),
+                             0, 0,
+                             white_pixels_left, white_pixels_top,
+                             width  - white_pixels_right,
+                             height - white_pixels_bottom)
+
     return love.graphics.newImage(trimmed_image_data)
 end
 
-
+-- find_successor:
+-- In a sequence (a table that consists of number values), checks until
+-- which index the sequence is without holes. For example:
+-- {0, 1, 2, 4, 5} --> 3
+--
+-- @params:  t @type: table
+--           i: The index from which to start counting. Defaults to 1.
+--              @type: number
+-- @returns: @type: number
 function find_successor (t, i)
+    local i = i or 1
+
     if t[i + 1] ~= t[i] + 1 then
         return i
     else
@@ -132,7 +184,18 @@ function find_successor (t, i)
     end
 end
 
+-- find_antecessor:
+-- In a sequence (a table that consists of number values), checks until
+-- which index the sequence is without holes FROM THE BACK. For example:
+-- {0, 1, 2, 4, 5} --> 4
+--
+-- @params:  t @type: table
+--           i: The index from which to start counting. Defaults to #t.
+--              @type: number
+-- @returns: @type: number
 function find_antecessor (t, i)
+    local i = i or #t
+
     if t[i - 1] ~= t[i] - 1 then
         return i
     else
@@ -149,13 +212,18 @@ end
 -- max_pair:
 -- Traverses a table and looks for the highest value. Returns this
 -- value and the corresponding index (key).
+--
+-- @params: t @type: table
+-- @returns:
+--     key @type: number
+--     max @type: number
 function max_pair (t)
     local key, max = 1, t[1]
 
     for k, v in ipairs(t) do
         if t[k] > max then
             key, max = k, v
-        end
+        end --inner if
     end
 
     return key, max
@@ -165,8 +233,8 @@ end
 -- invert_table:
 -- Creates a new table from a given table, swapping every key-value
 -- pair.
--- @params: t : table
--- @returns: table
+-- @params: t @type: table
+-- @returns: @type: table
 function invert_table (t)
     local s = {}
     for k, v in pairs(t) do
@@ -190,15 +258,17 @@ end
 -- Credit: http://richard.warburton.it/
 -- (Date retrieved: September 1st, 2015)
 --
--- @params: div, str : string
+-- @params: div, str
 --     div: The dividing string.
+--          @type: string
 --     str: The string to be divided.
+--          @type: string
 -- @returns: : table
 function explode (div, str)
     if (div == "") then return false end
     local pos, arr = 0, {}
     for st, sp in function () return string.find(str, div, pos, true) end do
-        table.insert(arr, string.sub(str, pos, st-1))
+        table.insert(arr, string.sub(str, pos, st - 1))
         pos = sp + 1
     end
     table.insert(arr, string.sub(str, pos))
