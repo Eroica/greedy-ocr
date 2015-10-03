af45c019aad25b52b51bdfdc03c07695ef459aa5
Seba <gnu.seba@outlook.com>
2015-10-03 21:14:48 +0200

    update Prototypes

--- _config.lua
+++ _config.lua
@@ -10,15 +10,15 @@ local config = {}
 
 config.DEBUG = true
 
-config.lexicon_filename = "_share/lexicon_1st_half.txt"
-config.corpus_filename  = "_share/mercurius_1st_half.txt"
+config.lexicon_filename = "_share/lexicon_until_april.txt"
+config.corpus_filename  = "_share/mercurius_until_april.txt"
 
 config.prototypes_directory = "_prototypes"
 config.pages_directory      = "_pages"
 
 config.BACKGROUND_COLOR = {127, 127, 127}
 
-config.automatically_split_segments = true
+config.automatically_split_segments = false
 config.MINIMUM_COMPONENT_WIDTH = 10
 config.SPLIT_THRESHOLD = 0.75
 
@@ -27,19 +27,20 @@ config.prototype_ranking = {
 }
 
 config.additional_prototypes = {
-    {"s", "_prototypes/_s_2.png"},
-    {"d", "_prototypes/_d_2.png"},
-    {"S", "_prototypes/_s_c.png"},
-    {"T", "_prototypes/_t_c.png"},
+    {"M", "_prototypes/_m_c.png"},
     {"J", "_prototypes/_j_c.png"},
-    {"K", "_prototypes/_k_c.png"},
-    {"H", "_prototypes/_h_c.png"},
-    {"G", "_prototypes/_g_c.png"},
-    {"F", "_prototypes/_f_c.png"},
+    {"P", "_prototypes/_p_c.png"},
+    {"ß", "_prototypes/_sz.png"},
+    {"T", "_prototypes/_t_c.png"},
+    {"N", "_prototypes/_n_c.png"},
     {"B", "_prototypes/_b_c.png"},
-    {"A", "_prototypes/_a_c.png"},
-    {"l", "_prototypes/_l_2.png"},
-    {"t", "_prototypes/_t_2.png"},
+    {"i", "_prototypes/_i_2.png"},
+    {"S", "_prototypes/_s_c.png"},
+    {"F", "_prototypes/_f_c.png"},
+    {"V", "_prototypes/_v_c.png"},
+    {"C", "_prototypes/_c_c.png"},
+    {"R", "_prototypes/_r_c.png"},
+    {"D", "_prototypes/_d_c.png"},
 }
 
 config.UNKNOWN_COMPONENTS = {[".*"] = true, [".?"] = true}
